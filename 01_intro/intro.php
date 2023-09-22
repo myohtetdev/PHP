288468b6a101e976d0b923f2385f4b38be8f01f5
Myo Htet <myohtettech1746@gmail.com>
2023-09-22 17:08:44 +0630

    First step of PHP

---
+++ 01_intro/intro.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+    // If you start using php you need to write with
+    // php ကိုစ‌ေရး‌ေတာ့မယ္ဆိုရင္
+    /*
+    
+    <?php  ?> 
+    
+    */
+    // All programming start greet with helloworld
+    // In php we can print text with echo()
+    echo "Hello World";
+    
+    
+    /**
+    *   Run php server
+    */
+    // The command to run php server 
+    //  php -S localhost:3000
+    /** you can give localhost address as you like */
+    
+    
+    
+?>
